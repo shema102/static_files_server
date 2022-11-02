26e01fb971966f349f53adfdd0e880b8b0459d04
shema102 <sergeishemshur@gmail.com>
2022-11-02 11:59:03 +0200

    Set port to 8080

--- main.py
+++ main.py
@@ -11,5 +11,5 @@ async def root():
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=80)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
 
