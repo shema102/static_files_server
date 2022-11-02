fbb7d83ba4419ea36435a008627b450f4f31d0f2
shema102 <sergeishemshur@gmail.com>
2022-11-02 11:49:39 +0200

    Launch on port 80

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+import uvicorn
 
 app = FastAPI()
  
 @app.get("/")
 async def root():
-    return {"message": "Static files server"}
+    return {"message": "Health - OK"}
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=80)
 
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-uvicorn main:app
+python3 main.py
